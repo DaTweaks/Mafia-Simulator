243ba04ecc558f49bb2574b143b606be1f247f0c
DaTweaks <51744514+DaTweaks@users.noreply.github.com>
2021-09-27 11:58:11 +0200

    feat(added game data folders)

--- MafiaSimulator/Program.cs
+++ MafiaSimulator/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace MafiaSimulator
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MafiaSimulator
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-                
+            FetchGameData();
+        }
+        
+        public static void FetchGameData()
+        {
+            var bankData = Directory.GetFiles(Path.GetFullPath("GameData/Banks"));
+            for (int i = 0; i < bankData.Length; i++)
+            {
+                using (FileStream tempFileStream = File.OpenRead(Path.GetFullPath(bankData[i])))
+                {
+                    byte[] tempArray = new byte[tempFileStream.Length];
+                    var tempText = new UTF8Encoding(true);
+                    while (tempFileStream.Read(tempArray,0,tempArray.Length) > 0)
+                    {
+                        Console.WriteLine(tempText.GetString(tempArray));
+                    }
+                }
+            }
         }
     }
 }
